7ddaca92a5c0cd41f18426f86aa6940b917be3f2
KenSato0527 <satoken527@gmail.com>
2020-01-09 17:25:23 +0900

    やすう反映(エラー残り)

--- Assets/DrinkGenerator.cs
+++ Assets/DrinkGenerator.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class DrinkGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject DrinkPrefab;
+    float span = 1.0f;
+    float delta = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(DrinkPrefab) as GameObject;
+            /*アイテムが枠範囲の指定
+             * int px = Random.Range(最小,最大);
+             * int py = Random.Range(最小,最大);
+             * go.transform.position = new Vector3 (px,py,1);
+             */
+        }
     }
 }
